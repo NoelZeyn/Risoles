226ae2d4f1dbb00202088dd1352c54909b4d2854
IStoum <ahmadakrom563@gmail.com>
2024-06-27 02:12:00 +0700

    perubahan drastis

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Validasi form sebelum submit
     const orderForm = document.querySelector('form');
     const menuSlider = document.querySelector('.menu');
-    const navMenu = document.querySelector('header nav ul');
     prevBtn.addEventListener('click', () => {
         menuSlider.scrollBy({
             left: -menuSlider.offsetWidth,
@@ -17,14 +15,15 @@ document.addEventListener('DOMContentLoaded', function() {
     });
     
     orderForm.addEventListener('submit', function(event) {
-        const name = document.getElementById('name').value.trim();
-        const email = document.getElementById('email').value.trim();
-        const order = document.getElementById('order').value.trim();
+        // const name = document.getElementById('name').value.trim();
+        // const email = document.getElementById('email').value.trim();
+        // const order = document.getElementById('order').value.trim();
 
-        if (!name || !email || !order) {
-            event.preventDefault();
-            alert("Semua kolom harus diisi.");
-        }
+        // if (!name || !email || !order) {
+        //     event.preventDefault();
+        //     alert("Semua kolom harus diisi.");
+        // }
+        alert("Maaf, fitur pemesanan sedang dalam maintenance sementara. Tolong hubungi kami melewati whatsapp untuk lebih lanjut");
     });
 
     // Scroll smooth untuk semua link dalam halaman
@@ -39,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function() {
             
             if (targetElement) {
                 window.scrollTo({
-                    top: targetElement.offsetTop - 100, // Sesuaikan dengan offset yang diinginkan
+                    top: targetElement.offsetTop - 100, 
                     behavior: 'smooth'
                 });
             }
